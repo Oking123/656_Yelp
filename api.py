b5e4b34ff2570b0109cfe326891a7afafcde221c
RanZang <ranzang8@gmail.com>
2020-03-27 21:57:26 -0400

    versin 4

--- api.py
+++ api.py
@@ -33,12 +33,14 @@ class UI:
 
     def main_menu(self):
         while True:
+            print('------------MAIN------------')
             print("0. Read newest posts")
             print("1. Post")
             print("2. Friend")
             print("3. Group")
             print("4. Topic")
             print("5. Signout")
+            print('----------------------------')
             option = input("")
             if option == '0':
                 self.read_post()
@@ -58,10 +60,12 @@ class UI:
 
     def group_menu(self):
         while True:
+            print('-----------GROUP------------')
             print("0. Show my groups")
             print("1. Join a group")
             print("2. Quit a group")
             print("3. Back")
+            print('----------------------------')
             option = input("")
             if option == '0':
                 cursor.execute("select group_name, group_list.group_id from group_join inner join group_list on group_list.group_id=group_join.group_id where user_id = '{}';".format(self.user_id))
@@ -82,10 +86,12 @@ class UI:
 
     def topic_menu(self):
         while True:
+            print('-----------TOPIC------------')
             print("0. Show my following topics")
             print("1. Add a topic")
             print("2. Delete a topic")
             print("3. Back")
+            print('----------------------------')
             option = input("")
             if option == '0':
                 cursor.execute("select business.business_id, name from follow_topic inner join business on follow_topic.business_id= business.business_id where user_id = '{}'".format(self.user_id))
@@ -123,10 +129,12 @@ class UI:
 
     def post_menu(self):
         while True:
+            print('------------POST------------')
             print("0. Show my posts")
             print("1. Post new review")
             print("2. Post new tip")
             print("3. Back")
+            print('----------------------------')
             option = input("")
             if option == '0':
                 self.mypost()
@@ -149,9 +157,11 @@ class UI:
 
     def read_post(self):
         while True:
-            print('0. Friend and Group')
-            print('1. Topic')
+            print('-------------READ-----------')
+            print('0. Post by friends and groups')
+            print('1. Post about followed topics')
             print('2. Back')
+            print('----------------------------')
             option = input('')
             if option == '0':
                 self.read_post_friend()
@@ -186,20 +196,20 @@ class UI:
         last_time = str(cursor.fetchone()[0])
         # select reviews and tips
         for item in friend_list:
-            cursor.execute('select user_id,review_date,name,review_text,review_id from ((select user_id, business_id, review_date,review_text,review_id from review where user_id = "{}") as A inner join (select business_id,name from business) as B on A.business_id = B.business_id);'.format(item))
+            cursor.execute('select user_id,review_date,name,review_text,review_id,useful from ((select user_id, business_id, review_date,review_text,review_id,useful from review where user_id = "{}") as A inner join (select business_id,name from business) as B on A.business_id = B.business_id);'.format(item))
             results = cursor.fetchall()
             if results:
                 for review in results:
                     if str(review[1])>refresh_time or str(review[1]) <last_time:
-                        temp=(review[0],str(review[1]),'REVIEW',review[2],review[3],review[4])
+                        temp=(review[0],str(review[1]),'REVIEW',review[2],review[3],review[4],review[5])
                         finalpost.append(temp)
         for item in friend_list:
-            cursor.execute('select user_id,tip_date,name,tip_text,tip_id from ((select user_id, business_id, tip_date,tip_text,tip_id from tip where user_id = "{}") as A inner join (select business_id,name from business) as B on A.business_id = B.business_id);'.format(item))
+            cursor.execute('select user_id,tip_date,name,tip_text,tip_id,compliment_count from ((select user_id, business_id, tip_date,tip_text,tip_id,compliment_count from tip where user_id = "{}") as A inner join (select business_id,name from business) as B on A.business_id = B.business_id);'.format(item))
             results = cursor.fetchall()
             if results:
                 for tip in results:
                     if str(tip[1]) >refresh_time or str(tip[1]) <last_time:
-                        temp=(tip[0],str(tip[1]),'TIP',tip[2],tip[3],tip[4])
+                        temp=(tip[0],str(tip[1]),'TIP',tip[2],tip[3],tip[4],tip[5])
                         finalpost.append(temp)
         finalpost = list(set(finalpost))
         finalpost.sort(key = lambda i:i[1],reverse=True)
@@ -210,14 +220,16 @@ class UI:
         while len(finalpost) > 10:
             quit2 = 0
             for i in range(10):
-                print('#{}'.format(i),finalpost[i][0],' TOPIC: {} '.format(finalpost[i][3]),finalpost[i][1])
+                print('#{}'.format(i),finalpost[i][0],' TOPIC: {} '.format(finalpost[i][3]),finalpost[i][1],'COMPLIMENT_COUNT:{}'.format(finalpost[i][6]))
                 print('')
                 print('{}:'.format(finalpost[i][2]),finalpost[i][4])
                 print('')
             while True:
+                print('----------------------------')
                 print('0. Compliment')
                 print('1. Next Page')
                 print('2. Back')
+                print('----------------------------')
                 option = input('')
                 if option == '0':
                     print('Choose the review or tip:')
@@ -253,13 +265,15 @@ class UI:
             return
         else:
             for i in range(len(finalpost)):
-                print('#{}'.format(i),finalpost[i][0],' TOPIC: {} '.format(finalpost[i][3]),finalpost[i][1])
+                print('#{}'.format(i),finalpost[i][0],' TOPIC: {} '.format(finalpost[i][3]),finalpost[i][1],'COMPLIMENT_COUNT:{}'.format(finalpost[i][6]))
                 print('')
                 print('{}:'.format(finalpost[i][2]),finalpost[i][4])
                 print('')
             while True:
+                print('----------------------------')
                 print('0. Compliment')
                 print('1. Back')
+                print('----------------------------')
                 option = input('')
                 if option == '0':
                     print('Choose the review or tip:')
@@ -304,24 +318,24 @@ class UI:
         last_time = str(cursor.fetchone()[0])
         # selec reviews and tips
         for item in topic_id:
-            cursor.execute('select user_id, business_id, review_date,review_text,review_id from review where business_id = "{}";'.format(item))
+            cursor.execute('select user_id, business_id, review_date,review_text,review_id,useful from review where business_id = "{}";'.format(item))
             result1 = cursor.fetchall()
             cursor.execute('select name from business where business_id = "{}"'.format(item))
             result2 = cursor.fetchone()[0]
             if result1 and result2:
                 for review in result1:
                     if str(review[2])>refresh_time or str(review[2]) <last_time:
-                        temp = (review[0], str(review[2]), 'REVIEW', result2, review[3],review[4])
+                        temp = (review[0], str(review[2]), 'REVIEW', result2, review[3],review[4],review[5])
                         finalpost.append(temp)
         for item in topic_id:
-            cursor.execute('select user_id, business_id, tip_date,tip_text,tip_id from tip where business_id = "{}";'.format(item))
+            cursor.execute('select user_id, business_id, tip_date,tip_text,tip_id,compliment_count from tip where business_id = "{}";'.format(item))
             result1 = cursor.fetchall()
             cursor.execute('select name from business where business_id = "{}"'.format(item))
             result2 = cursor.fetchone()[0]
             if result1 and result2:
                 for tip in result1:
                     if str(tip[2])>refresh_time or str(tip[2]) <last_time:
-                        temp = (tip[0], str(tip[2]), 'TIP', result2, tip[3],tip[4])
+                        temp = (tip[0], str(tip[2]), 'TIP', result2, tip[3],tip[4],tip[5])
                         finalpost.append(temp)
         finalpost = list(set(finalpost))
         finalpost.sort(key=lambda i: i[1],reverse = True)
@@ -332,14 +346,16 @@ class UI:
         while len(finalpost) > 10:
             quit2 = 0
             for i in range(10):
-                print('#{}'.format(i),finalpost[i][0],' TOPIC: {} '.format(finalpost[i][3]),finalpost[i][1])
+                print('#{}'.format(i),finalpost[i][0],' TOPIC: {} '.format(finalpost[i][3]),finalpost[i][1],'COMPLIMENT_COUNT:{}'.format(finalpost[i][6]))
                 print('')
                 print('{}:'.format(finalpost[i][2]),finalpost[i][4])
                 print('')
             while True:
+                print('----------------------------')
                 print('0. Compliment')
                 print('1. Next Page')
                 print('2. Back')
+                print('----------------------------')
                 option = input('')
                 if option == '0':
                     print('Choose the review or tip:')
@@ -375,13 +391,15 @@ class UI:
             return
         else:
             for i in range(len(finalpost)):
-                print('#{}'.format(i),finalpost[i][0],' TOPIC: {} '.format(finalpost[i][3]),finalpost[i][1])
+                print('#{}'.format(i),finalpost[i][0],' TOPIC: {} '.format(finalpost[i][3]),finalpost[i][1],'COMPLIMENT_COUNT:{}'.format(finalpost[i][6]))
                 print('')
                 print('{}:'.format(finalpost[i][2]),finalpost[i][4])
                 print('')
             while True:
+                print('----------------------------')
                 print('0. Compliment')
                 print('1. Back')
+                print('----------------------------')
                 option = input('')
                 if option == '0':
                     print('Choose the review or tip:')
@@ -444,6 +462,7 @@ class UI:
         except pymysql.err.IntegrityError:
             print("Error: unexpected business_id", file=sys.stderr)
             return -1
+        print('Successful operation')
         db.commit()
         return 1
 
@@ -456,15 +475,18 @@ class UI:
         except pymysql.err.IntegrityError:
             print("Error: unexpected business_id", file=sys.stderr)
             return -1
+        print('Successful operation')
         db.commit()
         return 1
 
     def add_friend(self):
         while True:
+            print('-----------FRIEND-----------')
             print('0. Show my friends')
             print('1. add a friend')
             print('2. delete a friend')
             print('3. back to menu')
+            print('----------------------------')
             option = input("")
             cursor.execute("select follower_id from friend_list where followed_id = '{}';".format(self.user_id))
             friends = cursor.fetchall()
@@ -488,10 +510,14 @@ class UI:
                     continue
                 else:
                     cursor.execute("insert into friend_list(followed_id, follower_id) value('{}','{}');".format(self.user_id, new_friend))
+                    print('Successful operation')
+                    db.commit()
             elif option == '2':
                 candi_friend = input("Delete friend:")
                 if candi_friend in friend_list:
                     cursor.execute("delete from friend_list where followed_id = '{}' and follower_id = '{}'".format(self.user_id, candi_friend))
+                    print('Successful operation')
+                    db.commit()
             elif option == '3':
                 break
 
@@ -509,6 +535,7 @@ class UI:
         cursor.execute("select max(group_id) from group_list;")
         group_number = cursor.fetchone()[0]
         cursor.execute("insert into group_join(user_id, group_id) value('{}',{});".format(self.user_id, group_number))
+        print('Successful operation')
         db.commit()
         return group_number
 
@@ -526,6 +553,7 @@ class UI:
         except pymysql.err.IntegrityError:
             print("ERROR: group_id not exists.", file=sys.stderr)
             return -1
+        print('Successful operation')
         db.commit()
         return 1
 
@@ -544,6 +572,7 @@ class UI:
             print("ERROR: group_id not exists.", file=sys.stderr)
             return -1
         cursor.execute("delete from group_join where user_id = '{}' and group_id = {}".format(self.user_id, group_id))
+        print('Successful operation')
         db.commit()
         return group_id
 
@@ -553,6 +582,7 @@ class UI:
         except pymysql.err.IntegrityError:
             print("Error: unknown business_id.", file = sys.stderr)
             return -1
+        print('Successful operation')
         db.commit()
         return 1
 
@@ -562,6 +592,7 @@ class UI:
         except pymysql.err.IntegrityError:
             print("Error: unexpected business_id", file = sys.stderr)
             return -1
+        print('Successful operation')
         db.commit()
         return 1
 
@@ -583,6 +614,7 @@ class UI:
             cursor.execute("update user set useful = useful - 1 where user_id = '{}'".format(user))
             cursor.execute("update review set useful = useful - 1 where review_id = '{}';".format(review_id))
         db.commit()
+        print('Successful operation')
         return 1
 
     def compliment_on_tip(self, tip_id, up_or_down):
@@ -602,6 +634,7 @@ class UI:
             user = cursor.fetchone()[0]
             cursor.execute("update user set useful = useful - 1 where user_id = '{}'".format(user))
             cursor.execute("update tip set compliment_count = compliment_count - 1 where tip_id = '{}';".format(tip_id))
+        print('Successful operation')
         db.commit()
         return 1
 
